4fd9e50cbd115d1b3605b934d683ca884ad50f40
Daniel Velazquez <daniel@gmail.com>
2016-05-22 21:38:31 +0200

    chat en directo

--- app/views/template.blade.php
+++ app/views/template.blade.php
@@ -19,6 +19,17 @@
         ga('send', 'pageview');
 
     </script>
+    <!-- Smartsupp Live Chat script -->
+    <script type="text/javascript">
+        var _smartsupp = _smartsupp || {};
+        _smartsupp.key = '4712de903068919401d4a384ed7f1018ecc2864b';
+        window.smartsupp||(function(d) {
+            var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+            s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+            c.type='text/javascript';c.charset='utf-8';c.async=true;
+            c.src='//www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+        })(document);
+    </script>
 
     <title>Buen provecho - ¿qué hay para comer?</title>
 
